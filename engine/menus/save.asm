aaaaaa123456789/pokecrystal16

Use 16-bit IDs in Bill's PC for boxes other than the current one
There are 280 Pokémon across all boxes in Bill's storage system, which would
by themselves overflow the conversion table twice if they were all different
species. Therefore, the conversion table only considers the currently loaded
box (in sBox); boxes in "cold storage" have their own ancillary true 16-bit
index list. (This also frees up a byte per Pokémon, since the first byte of
each Pokémon structure becomes useless this way.)

engine/menus/save.asm
@@ -482,8 +482,21 @@ SaveIndexTables:
 
 SaveBox:
 	call GetBoxAddress
+	push de
+	push af
 	call SaveBoxAddress
-	ret
+	pop af
+	call GetSRAMBank
+	pop hl
+	call ComputeSavedBoxIndexTable
+	call GetBoxPokemonIndexesAddress
+	call GetSRAMBank
+	ld d, h
+	ld e, l
+	ld hl, wBoxPartialData
+	ld bc, 2 * MONS_PER_BOX
+	call CopyBytes
+	jp CloseSRAM
 
 SaveChecksum:
 	ld hl, sSaveData
@@ -762,7 +775,24 @@ LoadIndexTables:
 LoadBox:
 	call GetBoxAddress
 	call LoadBoxAddress
-	ret
+	call GetBoxPokemonIndexesAddress
+	call GetSRAMBank
+	ld de, wBoxPartialData
+	ld bc, 2 * MONS_PER_BOX
+	call CopyBytes
+	ld a, BANK(sBox)
+	call GetSRAMBank
+	call ClearIndexesForLoadedBox
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wPokemonIndexTable)
+	ldh [rSVBK], a
+	; GC the table now that lots of entries are free
+	farcall PokemonTableGarbageCollection
+	pop af
+	ldh [rSVBK], a
+	call UpdateIndexesForLoadedBox
+	jp CloseSRAM
 
 VerifyChecksum:
 	ld hl, sSaveData
@@ -908,6 +938,23 @@ endr
 	pop af
 	ret
 
+GetBoxPokemonIndexesAddress:
+	ld a, [wCurBox]
+	ld e, a
+	ld d, 0
+	ld hl, BoxAddresses + 5 * NUM_BOXES
+	add hl, de
+	add hl, de
+	add hl, de
+	ld a, [hli]
+	push af
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	pop af
+	ret
+	ret
+
 SaveBoxAddress:
 ; Save box via wBoxPartialData.
 ; We do this in three steps because the size of wBoxPartialData is less than
@@ -983,6 +1030,48 @@ SaveBoxAddress:
 	pop hl
 	ret
 
+ComputeSavedBoxIndexTable:
+	push hl
+	ld a, [hl]
+	ld de, wBoxPartialData
+	and a
+	jr z, .empty_box
+	cp MONS_PER_BOX
+	jr c, .valid_count
+	ld a, MONS_PER_BOX
+.valid_count
+	ld bc, sBoxMons - sBox
+	add hl, bc
+	ld [wTempLoopCounter], a
+	ld c, BOXMON_STRUCT_LENGTH
+.loop
+	ld a, [hl]
+	add hl, bc
+	push hl
+	call GetPokemonIndexFromID
+	ld a, l
+	ld [de], a
+	inc de
+	ld a, h
+	ld [de], a
+	inc de
+	ld hl, wTempLoopCounter
+	dec [hl]
+	pop hl
+	jr nz, .loop
+.empty_box
+	pop hl
+	ld a, MONS_PER_BOX
+	sub [hl]
+	ret c
+	add a, a
+	ld h, d
+	ld l, e
+	ld c, a
+	xor a
+	ld b, a
+	jp ByteFill
+
 LoadBoxAddress:
 ; Load box via wBoxPartialData.
 ; We do this in three steps because the size of wBoxPartialData is less than
@@ -1046,6 +1135,59 @@ LoadBoxAddress:
 	pop hl
 	ret
 
+ClearIndexesForLoadedBox:
+	ld hl, sBoxMon1Species
+	ld bc, BOXMON_STRUCT_LENGTH
+	ld a, MONS_PER_BOX
+.loop
+	ld [hl], 0
+	add hl, bc
+	dec a
+	jr nz, .loop
+	ret
+
+UpdateIndexesForLoadedBox:
+	ld de, sBox
+	ld a, [de]
+	cp MONS_PER_BOX
+	jr c, .count_OK
+	ld a, MONS_PER_BOX
+	ld [de], a
+.count_OK
+	inc de
+	and a
+	jr z, .done
+	ld [wTempLoopCounter], a
+	ld bc, sBoxMon1Species
+	ld hl, wBoxPartialData - 1
+.loop
+	inc hl
+	ld a, [hli]
+	push hl
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	ld [bc], a
+	ld a, [de]
+	cp EGG
+	jr z, .is_egg
+	ld a, [bc]
+	ld [de], a
+.is_egg
+	ld hl, BOXMON_STRUCT_LENGTH
+	add hl, bc
+	ld b, h
+	ld c, l
+	inc de
+	ld hl, wTempLoopCounter
+	dec [hl]
+	pop hl
+	jr nz, .loop
+.done
+	ld a, -1
+	ld [de], a
+	ret
+
 EraseBoxes:
 	ld hl, BoxAddresses
 	ld c, NUM_BOXES
@@ -1085,6 +1227,22 @@ EraseBoxes:
 	pop bc
 	dec c
 	jr nz, .next
+	ld e, NUM_BOXES
+.index_loop
+	ld a, [hli]
+	call GetSRAMBank
+	ld a, [hli]
+	ld b, a
+	ld a, [hli]
+	push hl
+	ld h, a
+	ld l, b
+	xor a
+	ld bc, 2 * MONS_PER_BOX
+	call ByteFill
+	pop hl
+	dec e
+	jr nz, .index_loop
 	ret
 
 BoxAddresses:
@@ -1103,6 +1261,22 @@ BoxAddresses:
 	dbww BANK(sBox12), sBox12, sBox12End
 	dbww BANK(sBox13), sBox13, sBox13End
 	dbww BANK(sBox14), sBox14, sBox14End
+
+	; index addresses
+	dba sBox1PokemonIndexes
+	dba sBox2PokemonIndexes
+	dba sBox3PokemonIndexes
+	dba sBox4PokemonIndexes
+	dba sBox5PokemonIndexes
+	dba sBox6PokemonIndexes
+	dba sBox7PokemonIndexes
+	dba sBox8PokemonIndexes
+	dba sBox9PokemonIndexes
+	dba sBox10PokemonIndexes
+	dba sBox11PokemonIndexes
+	dba sBox12PokemonIndexes
+	dba sBox13PokemonIndexes
+	dba sBox14PokemonIndexes
 
 Checksum:
 	ld de, 0

engine/pokemon/bills_pc.asm
@@ -1114,11 +1114,12 @@ BillsPC_LoadMonStats:
 	ld hl, wBillsPC_ScrollPosition
 	add [hl]
 	ld e, a
-	ld d, $0
-	ld hl, wBillsPCPokemonList + 1 ; box number
-	add hl, de
-	add hl, de
-	add hl, de
+	add a, a
+	add a, a
+	ld c, a
+	ld b, 0
+	ld hl, wBillsPCPokemonList + 2 ; box number
+	add hl, bc
 	ld a, [hl]
 	and a
 	jr z, .party
@@ -1227,11 +1228,11 @@ BillsPC_RefreshTextboxes:
 	ld [hl], "┘"
 
 	ld a, [wBillsPC_ScrollPosition]
+	add a, a
+	add a, a
 	ld e, a
 	ld d, 0
 	ld hl, wBillsPCPokemonList
-	add hl, de
-	add hl, de
 	add hl, de
 	ld e, l
 	ld d, h
@@ -1249,6 +1250,7 @@ BillsPC_RefreshTextboxes:
 	inc de
 	inc de
 	inc de
+	inc de
 	pop af
 	dec a
 	jr nz, .loop
@@ -1259,6 +1261,12 @@ BillsPC_RefreshTextboxes:
 
 .PlaceNickname:
 	ld a, [de]
+	ld b, a
+	inc de
+	ld a, [de]
+	cp b
+	; both $0000 and $FFFF have two identical bytes, so this check works
+	jr nz, .get_nickname
 	and a
 	ret z
 	cp -1
@@ -1370,86 +1378,133 @@ BillsPC_RefreshTextboxes:
 .Placeholder:
 	db "-----@"
 
-copy_box_data: MACRO
-.loop\@
-	ld a, [hl]
-	cp -1
-	jr z, .done\@
-	and a
-	jr z, .done\@
-	ld [de], a
-	inc de
-	ld a, [wBillsPC_LoadedBox]
-	ld [de], a
-	inc de
-	ld a, [wd003]
-	ld [de], a
-	inc a
-	ld [wd003], a
-	inc de
-	inc hl
-	ld a, [wd004]
-	inc a
-	ld [wd004], a
-	jr .loop\@
-
-.done\@
-if \1
-	call CloseSRAM
-endc
-	ld a, -1
-	ld [de], a
-	ld a, [wd004]
-	inc a
-	ld [wBillsPC_NumMonsInBox], a
-ENDM
-
 CopyBoxmonSpecies:
 	xor a
 	ld hl, wBillsPCPokemonList
-	ld bc, 3 * 30
+	ld bc, 4 * 30
+	push hl
 	call ByteFill
-	ld de, wBillsPCPokemonList
-	xor a
-	ld [wd003], a
-	ld [wd004], a
+	pop hl
 	ld a, [wBillsPC_LoadedBox]
+	ld b, a
+	ld c, 0
 	and a
 	jr z, .party
 	cp NUM_BOXES + 1
 	jr z, .sBox
-	ld b, a
+
+	push bc
+	push hl
 	call GetBoxPointer
 	ld a, b
 	call GetSRAMBank
 	inc hl
-	copy_box_data 1
-	ret
+	ld d, h
+	ld e, l
+	pop hl
+	pop bc
+.box_loop
+	ld a, [de]
+	inc de
+	cp -1
+	jr z, .box_done
+	push hl
+	ld hl, EGG
+	cp l
+	call nz, BillsPC_GetSpeciesIndexForBoxSlot
+	ld a, h
+	ldh [hTemp], a
+	ld a, l
+	pop hl
+	ld [hli], a
+	ldh a, [hTemp]
+	ld [hli], a
+	ld a, b
+	ld [hli], a
+	ld a, c
+	ld [hli], a
+	inc c
+	jr .box_loop
 
-.party
-	ld hl, wPartySpecies
-	copy_box_data 0
+.box_done
+	call CloseSRAM ;preserves af
+.list_done
+	; expects a = $FF
+	ld [hli], a
+	ld [hl], a
+	ld a, c
+	inc a
+	ld [wBillsPC_NumMonsInBox], a
 	ret
 
 .sBox
 	ld a, BANK(sBox)
 	call GetSRAMBank
-	ld hl, sBoxSpecies
-	copy_box_data 1
+	ld de, sBoxSpecies
+	call .load_list
+	jp CloseSRAM
+
+.party
+	ld de, wPartySpecies
+.load_list
+	ld a, [de]
+	cp -1
+	jr z, .list_done
+	inc de
+	push hl
+	call GetPokemonIndexFromID
+	ld a, h
+	ldh [hTemp], a
+	ld a, l
+	pop hl
+	ld [hli], a
+	ldh a, [hTemp]
+	ld [hli], a
+	ld a, b
+	ld [hli], a
+	ld a, c
+	ld [hli], a
+	inc c
+	jr .load_list
+
+BillsPC_GetSpeciesIndexForBoxSlot:
+	; in: b: box, c: slot
+	; out: hl: species
+	; preserves bc, de
+	push bc
+	push de
+	dec b
+	ld a, c
+	ld c, b
+	ld b, a
+	farcall GetBoxMonPokemonIndexPointer
+	ldh a, [hSRAMBank]
+	ld c, a
+	ld a, b
+	call GetSRAMBank
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	ld a, c
+	call GetSRAMBank
+	pop de
+	pop bc
 	ret
 
 BillsPC_GetSelectedPokemonSpecies:
 	ld a, [wBillsPC_CursorPosition]
 	ld hl, wBillsPC_ScrollPosition
 	add [hl]
+	add a, a
+	add a, a
 	ld e, a
 	ld d, $0
 	ld hl, wBillsPCPokemonList
 	add hl, de
-	add hl, de
-	add hl, de
-	ld a, [hl]
-	ret
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	jp GetPokemonIDFromIndex
 
 BillsPC_UpdateSelectionCursor:
 	ld a, [wBillsPC_NumMonsInBox]
@@ -1740,13 +1795,16 @@ BillsPC_CopyMon:
 
 .box
 	ld b, a
+	ld a, [wCurPartyMon]
+	ld c, a
+	call BillsPC_GetSpeciesIndexForBoxSlot
+	call GetPokemonIDFromIndex
+	ld [wCurPartySpecies], a
+	ld l, LOCKED_MON_ID_CURRENT_MENU
+	call LockPokemonID
 	call GetBoxPointer
 	ld a, b
 	call GetSRAMBank
-	push hl
-	inc hl
-	call CopySpeciesToTemp
-	pop hl
 	push hl
 	ld bc, sBoxMonNicknames - sBox
 	add hl, bc
@@ -1762,6 +1820,9 @@ BillsPC_CopyMon:
 	ld bc, BOXMON_STRUCT_LENGTH
 	call CopyMonToTemp
 	call CloseSRAM
+	ld a, LOCKED_MON_ID_CURRENT_MENU
+	call GetLockedPokemonID
+	ld [wBufferMonSpecies], a
 	farcall CalcBufferMonStats
 	ret
 
@@ -2107,7 +2168,8 @@ CopySpeciesToTemp:
 	add hl, bc
 	ld a, [hl]
 	ld [wCurPartySpecies], a
-	ret
+	ld l, LOCKED_MON_ID_CURRENT_MENU
+	jp LockPokemonID
 
 CopyNicknameToTemp:
 	ld bc, MON_NAME_LENGTH

engine/printer/printer.asm
@@ -725,6 +725,8 @@ Printer_PrintBoxListSegment:
 	ld a, [de]
 	cp $ff
 	jp z, .finish
+	cp EGG
+	call nz, Printer_GetTrueBoxSpeciesIndex
 	ld [wNamedObjectIndexBuffer], a
 	ld [wCurPartySpecies], a
 
@@ -857,6 +859,40 @@ Printer_GetBoxMonSpecies:
 	add hl, de
 	ld e, l
 	ld d, h
+	pop hl
+	ret
+
+Printer_GetTrueBoxSpeciesIndex:
+	push hl
+	push af
+	ld a, [wWhichBoxToPrint]
+	ld hl, wCurBox
+	cp [hl]
+	pop hl
+	ld l, a
+	ld a, h
+	jr z, .done
+	push bc
+	ld c, l
+	ld a, [wAddrOfBoxToPrint]
+	cpl
+	add a, e
+	ld b, a
+	farcall GetBoxMonPokemonIndexPointer
+	ldh a, [hSRAMBank]
+	ld c, a
+	ld a, b
+	call GetSRAMBank
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	ld b, a
+	ld a, c
+	call GetSRAMBank
+	ld a, b
+	pop bc
+.done
 	pop hl
 	ret
 

ZoomTen/pokeoctober-new

Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

engine/menus/save.asm
@@ -491,8 +491,21 @@ SaveIndexTables:
 
 SaveBox:
 	call GetBoxAddress
+	push de
+	push af
 	call SaveBoxAddress
-	ret
+	pop af
+	call OpenSRAM
+	pop hl
+	call ComputeSavedBoxIndexTable
+	call GetBoxPokemonIndexesAddress
+	call OpenSRAM
+	ld d, h
+	ld e, l
+	ld hl, wBoxPartialData
+	ld bc, 2 * MONS_PER_BOX
+	call CopyBytes
+	jp CloseSRAM
 
 SaveChecksum:
 	ld hl, sSaveData
@@ -771,7 +784,24 @@ LoadIndexTables:
 LoadBox:
 	call GetBoxAddress
 	call LoadBoxAddress
-	ret
+	call GetBoxPokemonIndexesAddress
+	call OpenSRAM
+	ld de, wBoxPartialData
+	ld bc, 2 * MONS_PER_BOX
+	call CopyBytes
+	ld a, BANK(sBox)
+	call OpenSRAM
+	call ClearIndexesForLoadedBox
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wPokemonIndexTable)
+	ldh [rSVBK], a
+	; GC the table now that lots of entries are free
+	farcall PokemonTableGarbageCollection
+	pop af
+	ldh [rSVBK], a
+	call UpdateIndexesForLoadedBox
+	jp CloseSRAM
 
 VerifyChecksum:
 	ld hl, sSaveData
@@ -917,6 +947,23 @@ endr
 	pop af
 	ret
 
+GetBoxPokemonIndexesAddress:
+	ld a, [wCurBox]
+	ld e, a
+	ld d, 0
+	ld hl, BoxAddresses + 5 * NUM_BOXES
+	add hl, de
+	add hl, de
+	add hl, de
+	ld a, [hli]
+	push af
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	pop af
+	ret
+	ret
+
 SaveBoxAddress:
 ; Save box via wBoxPartialData.
 ; We do this in three steps because the size of wBoxPartialData is less than
@@ -992,6 +1039,48 @@ SaveBoxAddress:
 	pop hl
 	ret
 
+ComputeSavedBoxIndexTable:
+	push hl
+	ld a, [hl]
+	ld de, wBoxPartialData
+	and a
+	jr z, .empty_box
+	cp MONS_PER_BOX
+	jr c, .valid_count
+	ld a, MONS_PER_BOX
+.valid_count
+	ld bc, sBoxMons - sBox
+	add hl, bc
+	ld [wTempLoopCounter], a
+	ld c, BOXMON_STRUCT_LENGTH
+.loop
+	ld a, [hl]
+	add hl, bc
+	push hl
+	call GetPokemonIndexFromID
+	ld a, l
+	ld [de], a
+	inc de
+	ld a, h
+	ld [de], a
+	inc de
+	ld hl, wTempLoopCounter
+	dec [hl]
+	pop hl
+	jr nz, .loop
+.empty_box
+	pop hl
+	ld a, MONS_PER_BOX
+	sub [hl]
+	ret c
+	add a, a
+	ld h, d
+	ld l, e
+	ld c, a
+	xor a
+	ld b, a
+	jp ByteFill
+
 LoadBoxAddress:
 ; Load box via wBoxPartialData.
 ; We do this in three steps because the size of wBoxPartialData is less than
@@ -1055,6 +1144,59 @@ LoadBoxAddress:
 	pop hl
 	ret
 
+ClearIndexesForLoadedBox:
+	ld hl, sBoxMon1Species
+	ld bc, BOXMON_STRUCT_LENGTH
+	ld a, MONS_PER_BOX
+.loop
+	ld [hl], 0
+	add hl, bc
+	dec a
+	jr nz, .loop
+	ret
+
+UpdateIndexesForLoadedBox:
+	ld de, sBox
+	ld a, [de]
+	cp MONS_PER_BOX
+	jr c, .count_OK
+	ld a, MONS_PER_BOX
+	ld [de], a
+.count_OK
+	inc de
+	and a
+	jr z, .done
+	ld [wTempLoopCounter], a
+	ld bc, sBoxMon1Species
+	ld hl, wBoxPartialData - 1
+.loop
+	inc hl
+	ld a, [hli]
+	push hl
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	ld [bc], a
+	ld a, [de]
+	cp EGG
+	jr z, .is_egg
+	ld a, [bc]
+	ld [de], a
+.is_egg
+	ld hl, BOXMON_STRUCT_LENGTH
+	add hl, bc
+	ld b, h
+	ld c, l
+	inc de
+	ld hl, wTempLoopCounter
+	dec [hl]
+	pop hl
+	jr nz, .loop
+.done
+	ld a, -1
+	ld [de], a
+	ret
+
 EraseBoxes:
 	ld hl, BoxAddresses
 	ld c, NUM_BOXES
@@ -1094,6 +1236,22 @@ EraseBoxes:
 	pop bc
 	dec c
 	jr nz, .next
+	ld e, NUM_BOXES
+.index_loop
+	ld a, [hli]
+	call OpenSRAM
+	ld a, [hli]
+	ld b, a
+	ld a, [hli]
+	push hl
+	ld h, a
+	ld l, b
+	xor a
+	ld bc, 2 * MONS_PER_BOX
+	call ByteFill
+	pop hl
+	dec e
+	jr nz, .index_loop
 	ret
 
 BoxAddresses:
@@ -1102,6 +1260,25 @@ for n, 1, NUM_BOXES + 1
 	db BANK(sBox{d:n}) ; aka BANK(sBox{d:n}End)
 	dw sBox{d:n}, sBox{d:n}End
 endr
+	assert_table_length NUM_BOXES
+
+	; index addresses
+BoxIndexAddresses:
+	table_width 3, BoxIndexAddresses
+	dba sBox1PokemonIndexes
+	dba sBox2PokemonIndexes
+	dba sBox3PokemonIndexes
+	dba sBox4PokemonIndexes
+	dba sBox5PokemonIndexes
+	dba sBox6PokemonIndexes
+	dba sBox7PokemonIndexes
+	dba sBox8PokemonIndexes
+	dba sBox9PokemonIndexes
+	dba sBox10PokemonIndexes
+	dba sBox11PokemonIndexes
+	dba sBox12PokemonIndexes
+	dba sBox13PokemonIndexes
+	dba sBox14PokemonIndexes
 	assert_table_length NUM_BOXES
 
 Checksum:

engine/pokemon/bills_pc.asm
@@ -1114,11 +1114,12 @@ BillsPC_LoadMonStats:
 	ld hl, wBillsPC_ScrollPosition
 	add [hl]
 	ld e, a
-	ld d, 0
-	ld hl, wBillsPCPokemonList + BOXLIST_BOXNUM
-rept BOXLIST_SIZE
-	add hl, de
-endr
+	add a, a
+	add a, a
+	ld c, a
+	ld b, 0
+	ld hl, wBillsPCPokemonList + BOXLIST_BOXNUM ; box number
+	add hl, bc
 	ld a, [hl]
 	and a
 	jr z, .party
@@ -1227,12 +1228,12 @@ BillsPC_RefreshTextboxes:
 	ld [hl], '┘'
 
 	ld a, [wBillsPC_ScrollPosition]
+	add a, a
+	add a, a
 	ld e, a
 	ld d, 0
-	ld hl, wBillsPCPokemonList + BOXLIST_SPECIES
-rept BOXLIST_SIZE
+	ld hl, wBillsPCPokemonList
 	add hl, de
-endr
 	ld e, l
 	ld d, h
 	hlcoord 9, 4
@@ -1259,6 +1260,12 @@ endr
 
 .PlaceNickname:
 	ld a, [de]
+	ld b, a
+	inc de
+	ld a, [de]
+	cp b
+	; both $0000 and $FFFF have two identical bytes, so this check works
+	jr nz, .get_nickname
 	and a
 	ret z
 	cp -1
@@ -1370,86 +1377,133 @@ endr
 .Placeholder:
 	db "-----@"
 
-MACRO copy_box_data
-.loop\@
-	ld a, [hl]
-	cp -1
-	jr z, .done\@
-	and a
-	jr z, .done\@
-	ld [de], a ; BOXLIST_SPECIES
-	inc de
-	ld a, [wBillsPC_LoadedBox]
-	ld [de], a ; BOXLIST_BOXNUM
-	inc de
-	ld a, [wBillsPCTempListIndex]
-	ld [de], a ; BOXLIST_INDEX
-	inc a
-	ld [wBillsPCTempListIndex], a
-	inc de
-	inc hl
-	ld a, [wBillsPCTempBoxCount]
-	inc a
-	ld [wBillsPCTempBoxCount], a
-	jr .loop\@
-
-.done\@
-	if \1
-		call CloseSRAM
-	endc
-	ld a, -1
-	ld [de], a
-	ld a, [wBillsPCTempBoxCount]
-	inc a
-	ld [wBillsPC_NumMonsInBox], a
-ENDM
-
 CopyBoxmonSpecies:
 	xor a
 	ld hl, wBillsPCPokemonList
 	ld bc, BOXLIST_SIZE * MONS_PER_BOX_JP
+	push hl
 	call ByteFill
-	ld de, wBillsPCPokemonList
-	xor a
-	ld [wBillsPCTempListIndex], a
-	ld [wBillsPCTempBoxCount], a
+	pop hl
 	ld a, [wBillsPC_LoadedBox]
+	ld b, a
+	ld c, 0
 	and a
 	jr z, .party
 	cp NUM_BOXES + 1
 	jr z, .sBox
-	ld b, a
+
+	push bc
+	push hl
 	call GetBoxPointer
 	ld a, b
 	call OpenSRAM
 	inc hl
-	copy_box_data 1
-	ret
+	ld d, h
+	ld e, l
+	pop hl
+	pop bc
+.box_loop
+	ld a, [de]
+	inc de
+	cp -1
+	jr z, .box_done
+	push hl
+	ld hl, EGG
+	cp l
+	call nz, BillsPC_GetSpeciesIndexForBoxSlot
+	ld a, h
+	ldh [hTemp], a
+	ld a, l
+	pop hl
+	ld [hli], a
+	ldh a, [hTemp]
+	ld [hli], a
+	ld a, b
+	ld [hli], a
+	ld a, c
+	ld [hli], a
+	inc c
+	jr .box_loop
 
-.party
-	ld hl, wPartySpecies
-	copy_box_data 0
+.box_done
+	call CloseSRAM ;preserves af
+.list_done
+	; expects a = $FF
+	ld [hli], a
+	ld [hl], a
+	ld a, c
+	inc a
+	ld [wBillsPC_NumMonsInBox], a
 	ret
 
 .sBox
 	ld a, BANK(sBox)
 	call OpenSRAM
-	ld hl, sBoxSpecies
-	copy_box_data 1
+	ld de, sBoxSpecies
+	call .load_list
+	jp CloseSRAM
+
+.party
+	ld de, wPartySpecies
+.load_list
+	ld a, [de]
+	cp -1
+	jr z, .list_done
+	inc de
+	push hl
+	call GetPokemonIndexFromID
+	ld a, h
+	ldh [hTemp], a
+	ld a, l
+	pop hl
+	ld [hli], a
+	ldh a, [hTemp]
+	ld [hli], a
+	ld a, b
+	ld [hli], a
+	ld a, c
+	ld [hli], a
+	inc c
+	jr .load_list
+
+BillsPC_GetSpeciesIndexForBoxSlot:
+	; in: b: box, c: slot
+	; out: hl: species
+	; preserves bc, de
+	push bc
+	push de
+	dec b
+	ld a, c
+	ld c, b
+	ld b, a
+	farcall GetBoxMonPokemonIndexPointer
+	ldh a, [hSRAMBank]
+	ld c, a
+	ld a, b
+	call OpenSRAM
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	ld a, c
+	call OpenSRAM
+	pop de
+	pop bc
 	ret
 
 BillsPC_GetSelectedPokemonSpecies:
 	ld a, [wBillsPC_CursorPosition]
 	ld hl, wBillsPC_ScrollPosition
 	add [hl]
+	add a, a
+	add a, a
 	ld e, a
 	ld d, 0
-	ld hl, wBillsPCPokemonList + BOXLIST_SPECIES
-rept BOXLIST_SIZE
+	ld hl, wBillsPCPokemonList
 	add hl, de
-endr
-	ld a, [hl]
-	ret
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	jp GetPokemonIDFromIndex
 
 BillsPC_UpdateSelectionCursor:
 	ld a, [wBillsPC_NumMonsInBox]
@@ -1740,13 +1794,16 @@ BillsPC_CopyMon:
 
 .box
 	ld b, a
+	ld a, [wCurPartyMon]
+	ld c, a
+	call BillsPC_GetSpeciesIndexForBoxSlot
+	call GetPokemonIDFromIndex
+	ld [wCurPartySpecies], a
+	ld l, LOCKED_MON_ID_CURRENT_MENU
+	call LockPokemonID
 	call GetBoxPointer
 	ld a, b
 	call OpenSRAM
-	push hl
-	inc hl
-	call CopySpeciesToTemp
-	pop hl
 	push hl
 	ld bc, sBoxMonNicknames - sBox
 	add hl, bc
@@ -1762,6 +1819,9 @@ BillsPC_CopyMon:
 	ld bc, BOXMON_STRUCT_LENGTH
 	call CopyMonToTemp
 	call CloseSRAM
+	ld a, LOCKED_MON_ID_CURRENT_MENU
+	call GetLockedPokemonID
+	ld [wBufferMonSpecies], a
 	farcall CalcBufferMonStats
 	ret
 
@@ -2111,7 +2171,8 @@ CopySpeciesToTemp:
 	add hl, bc
 	ld a, [hl]
 	ld [wCurPartySpecies], a
-	ret
+	ld l, LOCKED_MON_ID_CURRENT_MENU
+	jp LockPokemonID
 
 CopyNicknameToTemp:
 	ld bc, MON_NAME_LENGTH

engine/printer/printer.asm
@@ -735,6 +735,8 @@ Printer_PrintBoxListSegment:
 	ld a, [de]
 	cp $ff
 	jp z, .finish
+	cp EGG
+	call nz, Printer_GetTrueBoxSpeciesIndex
 	ld [wNamedObjectIndex], a
 	ld [wCurPartySpecies], a
 
@@ -867,6 +869,40 @@ Printer_GetBoxMonSpecies:
 	add hl, de
 	ld e, l
 	ld d, h
+	pop hl
+	ret
+
+Printer_GetTrueBoxSpeciesIndex:
+	push hl
+	push af
+	ld a, [wWhichBoxToPrint]
+	ld hl, wCurBox
+	cp [hl]
+	pop hl
+	ld l, a
+	ld a, h
+	jr z, .done
+	push bc
+	ld c, l
+	ld a, [wAddrOfBoxToPrint]
+	cpl
+	add a, e
+	ld b, a
+	farcall GetBoxMonPokemonIndexPointer
+	ldh a, [hSRAMBank]
+	ld c, a
+	ld a, b
+	call OpenSRAM
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	ld b, a
+	ld a, c
+	call OpenSRAM
+	ld a, b
+	pop bc
+.done
 	pop hl
 	ret
 