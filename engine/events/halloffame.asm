aaaaaa123456789/pokecrystal16

Update the Hall of Fame data to use 16-bit IDs
For similar reasons to the PC boxes, HOF data needs to use true 16-bit indexes
instead of going through the conversion table — HOF data caps at 180 Pokémon,
which is on its own well above the table's capacity. Since this data is only
used in very specific circumstances, it is acceptable to simply expand each ID
to be two bytes long.

engine/events/halloffame.asm
@@ -92,11 +92,12 @@ AnimateHallOfFame:
 	ld a, [wHallOfFameMonCounter]
 	cp PARTY_LENGTH
 	jr nc, .done
-	ld hl, wHallOfFameTempMon1
+	ld hl, wHallOfFameTempMon1 + 1
 	ld bc, wHallOfFameTempMon1End - wHallOfFameTempMon1
 	call AddNTimes
-	ld a, [hl]
-	cp -1
+	ld a, [hld]
+	and [hl]
+	inc a
 	jr z, .done
 	push hl
 	call AnimateHOFMonEntrance
@@ -135,7 +136,7 @@ AnimateHallOfFame:
 
 GetHallOfFameParty:
 	ld hl, wHallOfFamePokemonList
-	ld bc, wHallOfFamePokemonListEnd - wHallOfFamePokemonList + 1
+	ld bc, HOF_LENGTH
 	xor a
 	call ByteFill
 	ld a, [wHallOfFameCount]
@@ -168,6 +169,11 @@ GetHallOfFameParty:
 	ld hl, MON_SPECIES
 	add hl, bc
 	ld a, [hl]
+	call GetPokemonIndexFromID
+	ld a, l
+	ld [de], a
+	inc de
+	ld a, h
 	ld [de], a
 	inc de
 
@@ -217,6 +223,8 @@ GetHallOfFameParty:
 .done
 	ld a, -1
 	ld [de], a
+	inc de
+	ld [de], a
 	ret
 
 AnimateHOFMonEntrance:
@@ -225,6 +233,12 @@ AnimateHOFMonEntrance:
 	farcall ResetDisplayBetweenHallOfFameMons
 	pop hl
 	ld a, [hli]
+	push hl
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	pop hl
+	inc hl
 	ld [wTempMonSpecies], a
 	ld [wCurPartySpecies], a
 	inc hl
@@ -346,11 +360,12 @@ _HallOfFamePC:
 	ld a, [wHallOfFameMonCounter]
 	cp PARTY_LENGTH
 	jr nc, .fail
-	ld hl, wHallOfFameTempMon1
+	ld hl, wHallOfFameTempMon1 + 1
 	ld bc, wHallOfFameTempMon1End - wHallOfFameTempMon1
 	call AddNTimes
-	ld a, [hl]
-	cp -1
+	ld a, [hld]
+	and [hl]
+	inc a
 	jr nz, .okay
 
 .fail
@@ -408,7 +423,7 @@ LoadHOFTeam:
 	cp NUM_HOF_TEAMS
 	jr nc, .invalid
 	ld hl, sHallOfFame
-	ld bc, wHallOfFameTempEnd - wHallOfFameTemp + 1
+	ld bc, HOF_LENGTH
 	call AddNTimes
 	ld a, BANK(sHallOfFame)
 	call GetSRAMBank
@@ -416,7 +431,7 @@ LoadHOFTeam:
 	and a
 	jr z, .absent
 	ld de, wHallOfFameTemp
-	ld bc, wHallOfFameTempEnd - wHallOfFameTemp + 1
+	ld bc, HOF_LENGTH
 	call CopyBytes
 	call CloseSRAM
 	and a
@@ -433,6 +448,12 @@ DisplayHOFMon:
 	xor a
 	ldh [hBGMapMode], a
 	ld a, [hli]
+	push hl
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	pop hl
+	inc hl
 	ld [wTempMonSpecies], a
 	ld a, [hli]
 	ld [wTempMonID], a
@@ -461,7 +482,6 @@ DisplayHOFMon:
 	call Textbox
 	ld a, [wTempMonSpecies]
 	ld [wCurPartySpecies], a
-	ld [wDeciramBuffer], a
 	ld hl, wTempMonDVs
 	predef GetUnownLetter
 	xor a
@@ -475,10 +495,21 @@ DisplayHOFMon:
 	ld a, "№"
 	ld [hli], a
 	ld [hl], "<DOT>"
+	ld a, [wCurPartySpecies]
+	call GetPokemonIndexFromID
+	ld a, l
+	ld l, h
+	ld h, a
+	push hl
+	ld hl, sp + 0
+	ld d, h
+	ld e, l
 	hlcoord 3, 13
-	ld de, wDeciramBuffer
-	lb bc, PRINTNUM_LEADINGZEROS | 1, 3
+	lb bc, PRINTNUM_LEADINGZEROS | 2, 3
 	call PrintNum
+	pop hl
+	ld a, [wCurPartySpecies]
+	ld [wNamedObjectIndexBuffer], a
 	call GetBasePokemonName
 	hlcoord 7, 13
 	call PlaceString

ZoomTen/pokeoctober-new

Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

engine/events/halloffame.asm
@@ -92,11 +92,12 @@ AnimateHallOfFame:
 	ld a, [wHallOfFameMonCounter]
 	cp PARTY_LENGTH
 	jr nc, .done
-	ld hl, wHallOfFameTempMon1
+	ld hl, wHallOfFameTempMon1 + 1
 	ld bc, wHallOfFameTempMon1End - wHallOfFameTempMon1
 	call AddNTimes
-	ld a, [hl]
-	cp -1
+	ld a, [hld]
+	and [hl]
+	inc a
 	jr z, .done
 	push hl
 	call AnimateHOFMonEntrance
@@ -135,7 +136,7 @@ AnimateHallOfFame:
 
 GetHallOfFameParty:
 	ld hl, wHallOfFamePokemonList
-	ld bc, wHallOfFamePokemonListEnd - wHallOfFamePokemonList + 1
+	ld bc, HOF_LENGTH
 	xor a
 	call ByteFill
 	ld a, [wHallOfFameCount]
@@ -168,6 +169,11 @@ GetHallOfFameParty:
 	ld hl, MON_SPECIES
 	add hl, bc
 	ld a, [hl]
+	call GetPokemonIndexFromID
+	ld a, l
+	ld [de], a
+	inc de
+	ld a, h
 	ld [de], a
 	inc de
 
@@ -217,6 +223,8 @@ GetHallOfFameParty:
 .done
 	ld a, -1
 	ld [de], a
+	inc de
+	ld [de], a
 	ret
 
 AnimateHOFMonEntrance:
@@ -225,6 +233,12 @@ AnimateHOFMonEntrance:
 	farcall ResetDisplayBetweenHallOfFameMons
 	pop hl
 	ld a, [hli]
+	push hl
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	pop hl
+	inc hl
 	ld [wTempMonSpecies], a
 	ld [wCurPartySpecies], a
 	inc hl
@@ -346,11 +360,12 @@ _HallOfFamePC:
 	ld a, [wHallOfFameMonCounter]
 	cp PARTY_LENGTH
 	jr nc, .fail
-	ld hl, wHallOfFameTempMon1
+	ld hl, wHallOfFameTempMon1 + 1
 	ld bc, wHallOfFameTempMon1End - wHallOfFameTempMon1
 	call AddNTimes
-	ld a, [hl]
-	cp -1
+	ld a, [hld]
+	and [hl]
+	inc a
 	jr nz, .okay
 
 .fail
@@ -409,7 +424,7 @@ LoadHOFTeam:
 	cp NUM_HOF_TEAMS
 	jr nc, .invalid
 	ld hl, sHallOfFame
-	ld bc, wHallOfFameTempEnd - wHallOfFameTemp + 1
+	ld bc, HOF_LENGTH
 	call AddNTimes
 	ld a, BANK(sHallOfFame)
 	call OpenSRAM
@@ -417,7 +432,7 @@ LoadHOFTeam:
 	and a
 	jr z, .absent
 	ld de, wHallOfFameTemp
-	ld bc, wHallOfFameTempEnd - wHallOfFameTemp + 1
+	ld bc, HOF_LENGTH
 	call CopyBytes
 	call CloseSRAM
 	and a
@@ -434,6 +449,12 @@ DisplayHOFMon:
 	xor a
 	ldh [hBGMapMode], a
 	ld a, [hli]
+	push hl
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	pop hl
+	inc hl
 	ld [wTempMonSpecies], a
 	ld a, [hli]
 	ld [wTempMonID], a
@@ -462,7 +483,6 @@ DisplayHOFMon:
 	call Textbox
 	ld a, [wTempMonSpecies]
 	ld [wCurPartySpecies], a
-	ld [wTextDecimalByte], a
 	ld hl, wTempMonDVs
 	predef GetUnownLetter
 	xor a
@@ -476,10 +496,21 @@ DisplayHOFMon:
 	ld a, '№'
 	ld [hli], a
 	ld [hl], '<DOT>'
+	ld a, [wCurPartySpecies]
+	call GetPokemonIndexFromID
+	ld a, l
+	ld l, h
+	ld h, a
+	push hl
+	ld hl, sp + 0
+	ld d, h
+	ld e, l
 	hlcoord 3, 13
-	ld de, wTextDecimalByte
-	lb bc, PRINTNUM_LEADINGZEROS | 1, 3
+	lb bc, PRINTNUM_LEADINGZEROS | 2, 3
 	call PrintNum
+	pop hl
+	ld a, [wCurPartySpecies]
+	ld [wNamedObjectIndex], a
 	call GetBasePokemonName
 	hlcoord 7, 13
 	call PlaceString